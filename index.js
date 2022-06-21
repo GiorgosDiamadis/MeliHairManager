512033d45c05db8cc0508c73f4afaabdeb44faa6
GiorgosDiamadis <diamantisgiorgos2@gmail.com>
2022-06-22 00:16:57 +0300

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const toggler = document.getElementById("btn");
+
+toggler.addEventListener("click", (e) => {
+  const navbar = document.querySelector("nav");
+  const expanded = e.target.parentElement.getAttribute("aria-expanded");
+  if (expanded === "true") {
+    navbar.classList.remove("transparent");
+  } else {
+    setTimeout(() => {
+      navbar.classList.add("transparent");
+    }, 320);
+  }
+});
+
+var myModal = document.getElementById("myModal");
+
+myModal.addEventListener("shown.bs.modal", function () {});
