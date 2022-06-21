00dc5528143665223eebd3b40d6d0a97a78a2b88
GiorgosDiamadis <diamantisgiorgos2@gmail.com>
2022-06-22 00:57:33 +0300

    commt

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const toggler = document.getElementById("btn");
 
-toggler.addEventListener("click", (e) => {
-  const navbar = document.querySelector("nav");
-  const expanded = e.target.parentElement.getAttribute("aria-expanded");
-  if (expanded === "true") {
-    navbar.classList.remove("transparent");
-  } else {
-    setTimeout(() => {
-      navbar.classList.add("transparent");
-    }, 320);
-  }
-});
+// toggler.addEventListener("click", (e) => {
+//   const navbar = document.querySelector("nav");
+//   const expanded = e.target.parentElement.getAttribute("aria-expanded");
+//   if (expanded === "true") {
+//     navbar.classList.remove("transparent");
+//   } else {
+//     setTimeout(() => {
+//       navbar.classList.add("transparent");
+//     }, 320);
+//   }
+// });
 
 var myModal = document.getElementById("myModal");
 
